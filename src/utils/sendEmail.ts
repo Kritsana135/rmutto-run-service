60775a17591204059279991efab0924fa606b982
Kritsana135 <kritsana.syg@gmail.com>
2022-02-09 10:21:05 +0700

    add recovery email template

--- src/utils/sendEmail.ts
+++ src/utils/sendEmail.ts
@@ -1,55 +1,64 @@
+import fs from "fs";
 import nodemailer from "nodemailer";
 import Mail from "nodemailer/lib/mailer";
-import {v4} from "uuid";
-import {redis} from "./redis";
-import {EmailConfig, ExpireTime} from "../config/appConfig";
+import { v4 } from "uuid";
+import { EmailConfig, ExpireTime } from "../config/appConfig";
+import { redis } from "./redis";
 
+export async function sendEmail(mail: Mail.Options) {
+  const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: EmailConfig.user,
+      pass: EmailConfig.pass,
+    },
+  });
 
-export async function sendEmail(mail:Mail.Options) {
+  const info = await transporter.sendMail(mail);
 
-    const transporter = nodemailer.createTransport({
-        host: 'smtp.gmail.com',
-        port: 587,
-        secure: false, // true for 465, false for other ports
-        auth: {
-            user: EmailConfig.user,
-            pass: EmailConfig.pass,
-        },
-    });
-
-    const info = await transporter.sendMail(mail);
-
-    console.log("Message sent: %s", info.messageId);
-    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+  console.log("Message sent: %s", info.messageId);
+  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
 }
 
+export const createConfirmEmail = (
+  receiverEmail: string,
+  confirmUrl: string
+): Mail.Options => ({
+  from: EmailConfig.senderEmail, // sender address
+  to: receiverEmail, // list of receivers
+  subject: "RMUTTO RUN Email Verify", // Subject line
+  html: `<a href='${confirmUrl}'>${confirmUrl}</a>`, // html body
+});
 
-
-export const createConfirmEmail = (receiverEmail:string,confirmUrl: string):Mail.Options => ({
-    from: EmailConfig.senderEmail, // sender address
-    to: receiverEmail, // list of receivers
-    subject: "RMUTTO RUN Email Verify", // Subject line
-    html: `<a href='${confirmUrl}'>${confirmUrl}</a>`, // html body
-})
-
-export const createResetPassEmail = (receiverEmail:string,resetPassUrl: string):Mail.Options => ({
+export const createResetPassEmail = (
+  receiverEmail: string,
+  resetPassUrl: string
+): Mail.Options => {
+  const file = fs.readFileSync("src/email/recover.html");
+  const html = file
+    .toString()
+    .replace("{{%RESET_URL%}}", resetPassUrl)
+    .replace("{{%CLIENT_URL%}}", process.env.WEB_ENDPOINT!);
+  return {
     from: EmailConfig.senderEmail, // sender address
     to: receiverEmail, // list of receivers
     subject: "RMUTTO RUN Reset Password", // Subject line
-    html: `<a href='${resetPassUrl}'>${resetPassUrl}</a>`, // html body
-})
+    html,
+  };
+};
 
 export const createConfirmationUrl = async (userId: string) => {
-    const token = v4();
-    await redis.set(token, userId, "ex", ExpireTime.verifyEmail); // 1 day expiration
+  const token = v4();
+  await redis.set(token, userId, "ex", ExpireTime.verifyEmail); // 1 day expiration
 
-    return `${EmailConfig.verifyEmailUrl}/${token}`;
+  return `${EmailConfig.verifyEmailUrl}/${token}`;
 };
 
 export const createResetUrl = async (userId: string) => {
-    const token = v4();
-    await redis.set(token, userId, "ex", ExpireTime.resetEmail); // 1 day expiration
+  const token = v4();
+  await redis.set(token, userId, "ex", ExpireTime.resetEmail); // 1 day expiration
 
-    return `${EmailConfig.resetEmailUrl}/${token}`;
+  return `${EmailConfig.resetEmailUrl}/${token}`;
 };
-

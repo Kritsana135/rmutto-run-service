d45be77b30e96c4e263d8901c22035c09122cf0f
Kritsana135 <61050135@kmitl.ac.th>
2022-09-13 16:19:58 +0700

    update orm config

--- ormconfig.js
+++ ormconfig.js
@@ -1,6 +1,6 @@
 const dotenv = require("dotenv");
 
-const isDev = process.env.NODE_ENV == "dev"
+const isDev = process.env.NODE_ENV == "dev";
 
 dotenv.config({
   path: process.env.NODE_ENV == "dev" ? ".env.dev" : ".env.production",
@@ -10,21 +10,30 @@ console.log(
   process.env.TYPEORM_ENTITIES,
   process.env.DB_HOST,
   process.env.NODE_ENV,
-  process.env.NODE_ENV === "dev"
+  process.env.NODE_ENV === "dev",
+  isDev
 );
 
 module.exports = {
   type: "postgres",
   host: process.env.DB_HOST,
-  port: 5432,
+  port: process.env.DB_PORT,
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
+  database: process.env.DB_NAME,
   synchronize: true,
   logging: false,
   extra: {
     ssl: !isDev,
   },
-  entities: [process.env.TYPEORM_ENTITIES],
-  migrations: [process.env.TYPEORM_MIGRATION],
+  entities: [
+    `${process.env.BUILD_DIRECTORY || "src"}/entity/**/*.${
+      process.env.BUILD_TARGET || "ts"
+    }`,
+  ],
+  migrations: [
+    `${process.env.BUILD_DIRECTORY || "src"}/migration/**/*.${
+      process.env.BUILD_TARGET || "ts"
+    }`,
+  ],
 };
